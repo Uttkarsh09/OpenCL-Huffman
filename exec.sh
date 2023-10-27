e56ca7cba601a1b2c2a4265bd0770e34e7cc805c
Uttkarsh Patel <uttkarshpatel9@gmail.com>
2023-10-27 10:21:28 +0530

    fixed compression bug

--- exec.sh
+++ exec.sh
@@ -1,2 +1,4 @@
 rm -f executionLog.log
 ./linux.sh > executionLog.log
+
+# cp -fv /home/uttkarsh/VIT/4th_Year/Major_Project/Code/OpenCLHuffman/test/processed.txt.huff /home/uttkarsh/VIT/4th_Year/Major_Project/Code/GapArrCPU/CPU/test/processed.txt.huff
